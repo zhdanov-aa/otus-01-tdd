1e19b91a8d8ecbfefa7565a77a19a7d01c09f5aa
zhdanov-aa <zhdanov_aa@mail.ru>
2025-01-24 16:17:30 +0400

    Исправление ошибок

--- QuadraticEquation.cpp
+++ QuadraticEquation.cpp
@@ -4,12 +4,12 @@
 
 std::vector<double> QuadraticEquation::solve(double a, double b, double c)
 {
+    const double epsilon = 0.0000001;
+
     if (abs(a) < epsilon)
     {
         throw std::invalid_argument("a == 0");
     }
-    
-    const double epsilon = 0.0000001;
     
     double d = b*b-4*a*c;
 
