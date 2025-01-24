82ef1deee4b0edafe196bbb89c09c6851ed4bdae
zhdanov-aa <zhdanov_aa@mail.ru>
2025-01-24 14:50:47 +0400

    Исправлены ошибки в тесте

--- tests/QuadraticEquation_test.cpp
+++ tests/QuadraticEquation_test.cpp
@@ -5,8 +5,6 @@
 
 using namespace testing;
 
-const double epsilon = 0.0000001;
-
 TEST(QuadraticEquation, solve_no_roots)
 {
     ASSERT_TRUE(QuadraticEquation::solve(1, 0, 1).empty());
@@ -15,7 +13,7 @@ TEST(QuadraticEquation, solve_no_roots)
 TEST(QuadraticEquation, solve_two_roots)
 {
     auto result = QuadraticEquation::solve(1, 0, -1);
-    ASSERT_TRUE(result.size() == 2);
-    ASSERT_TRUE(abs(result[0] - 1) < epsilon);
-    ASSERT_TRUE(abs(result[1] + 1) < epsilon);
+    ASSERT_EQ(result.size(), 2);
+    ASSERT_DOUBLE_EQ(result[0], 1);
+    ASSERT_DOUBLE_EQ(result[1], -1);
 }
