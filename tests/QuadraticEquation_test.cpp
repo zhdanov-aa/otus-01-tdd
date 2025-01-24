5efe94a0e41c8682086ec8309b8e2731dd551907
zhdanov-aa <zhdanov_aa@mail.ru>
2025-01-24 16:06:14 +0400

    Добавлен тест a == 0

--- tests/QuadraticEquation_test.cpp
+++ tests/QuadraticEquation_test.cpp
@@ -18,9 +18,14 @@ TEST(QuadraticEquation, solve_two_roots)
     ASSERT_DOUBLE_EQ(result[1], -1);
 }
 
-TEST(QuadraticEquation, solve_one_roots)
+TEST(QuadraticEquation, solve_one_root)
 {
     auto result = QuadraticEquation::solve(1, 2, 1);
     ASSERT_EQ(result.size(), 1);
     ASSERT_DOUBLE_EQ(result[0], -1);
+}
+
+TEST(QuadraticEquation, solve_a_zero_exception)
+{
+    EXPECT_ANY_THROW(QuadraticEquation::solve(0, 1, 1));
 }
