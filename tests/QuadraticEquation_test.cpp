f6d1aa5749430f74ab5b8024eb510c459dd2891e
zhdanov-aa <zhdanov_aa@mail.ru>
2025-01-24 11:55:54 +0400

    Добавлена проверка на то, что уравнение x^2+1=0 не имеет корней

---
+++ tests/QuadraticEquation_test.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+
+using namespace testing;
+
+TEST(QuadraticEquation, solve_no_roots)
+{
+    ASSERT_TRUE(QuadraticEquation::solve(1, 0, 1).empty());
+}
