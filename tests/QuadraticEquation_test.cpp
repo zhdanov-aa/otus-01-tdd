a957abdfaf45a40f7963c03ca5b9710f576ef436
zhdanov-aa <zhdanov_aa@mail.ru>
2025-01-24 14:37:11 +0400

    Добавлена проверка на два корня кратности 1

--- tests/QuadraticEquation_test.cpp
+++ tests/QuadraticEquation_test.cpp
@@ -1,10 +1,21 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock-matchers.h>
+#include <math.h>
 #include "../QuadraticEquation.h"
 
 using namespace testing;
 
+const epsilon = 0.0000001;
+
 TEST(QuadraticEquation, solve_no_roots)
 {
     ASSERT_TRUE(QuadraticEquation::solve(1, 0, 1).empty());
+}
+
+TEST(QuadraticEquation, solve_two_roots)
+{
+    auto result = QuadraticEquation::solve(1, 0, -1);
+    ASSERT_TRUE(result.size() == 2);
+    ASSERT_TRUE(abs(result[0] - 1) < epsilon);
+    ASSERT_TRUE(abs(result[1] + 1) < epsilon);
 }
