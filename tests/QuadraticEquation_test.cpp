23526862c9670dfb39942247c55a7b354897c16b
zhdanov-aa <zhdanov_aa@mail.ru>
2025-01-24 16:58:54 +0400

    Добавлен тест на проверку аргументов на NAN и INFINITY

--- tests/QuadraticEquation_test.cpp
+++ tests/QuadraticEquation_test.cpp
@@ -36,3 +36,18 @@ TEST(QuadraticEquation, solve_D_less_epsilon)
     ASSERT_EQ(result.size(), 1);
     ASSERT_DOUBLE_EQ(result[0], -1);
 }
+
+TEST(QuadraticEquation, solve_nan_inf_exception)
+{
+    EXPECT_ANY_THROW(QuadraticEquation::solve(NAN, 3, 1));
+    EXPECT_ANY_THROW(QuadraticEquation::solve(INFINITY, 3, 1));
+    EXPECT_ANY_THROW(QuadraticEquation::solve(-INFINITY, 3, 1));
+
+    EXPECT_ANY_THROW(QuadraticEquation::solve(1, NAN, 1));
+    EXPECT_ANY_THROW(QuadraticEquation::solve(1, INFINITY, 1));
+    EXPECT_ANY_THROW(QuadraticEquation::solve(1, -INFINITY, 1));
+
+    EXPECT_ANY_THROW(QuadraticEquation::solve(1, 3, NAN));
+    EXPECT_ANY_THROW(QuadraticEquation::solve(1, 3, INFINITY));
+    EXPECT_ANY_THROW(QuadraticEquation::solve(1, 3, -INFINITY));
+}
