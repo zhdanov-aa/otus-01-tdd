32a036970318c9204b1f2982d25970cf731abefb
zhdanov-aa <zhdanov_aa@mail.ru>
2025-01-24 17:09:20 +0400

    В solve() реализована проверка на nan и infinity

--- QuadraticEquation.cpp
+++ QuadraticEquation.cpp
@@ -4,11 +4,26 @@
 
 std::vector<double> QuadraticEquation::solve(double a, double b, double c)
 {
+    if (!isfinite(a))
+    {
+        throw std::invalid_argument("A is not finite number");
+    }
+
+    if (!isfinite(b))
+    {
+        throw std::invalid_argument("B is not finite number");
+    }
+
+    if (!isfinite(c))
+    {
+        throw std::invalid_argument("C is not finite number");
+    }
+    
     const double epsilon = 0.0000001;
 
     if (abs(a) < epsilon)
     {
-        throw std::invalid_argument("a == 0");
+        throw std::invalid_argument("A == 0");
     }
     
     double d = b*b-4*a*c;
