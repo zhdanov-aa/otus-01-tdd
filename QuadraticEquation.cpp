6c976341ac2daba18e5b17581b07ef0e5adb835a
zhdanov-aa <zhdanov_aa@mail.ru>
2025-01-24 14:14:25 +0400

    Реализована функция solve()

--- QuadraticEquation.cpp
+++ QuadraticEquation.cpp
@@ -1,6 +1,25 @@
+#include <math.h>
 #include "QuadraticEquation.h"
 
 std::vector<double> QuadraticEquation::solve(double a, double b, double c)
 {
+    const double epsilon = 0.0000001;
+    
+    double d = b*b-4*a*c;
 
+    if(abs(d) < epsilon)
+    {
+        vector<double> result { (-1 * b / 2 / a) };
+        return result;
+    }
+    else if (d < 0)
+    {
+        return vector<double>();
+    }
+    else
+    {
+        double sqrt_d = sqrt(d);
+        vector<double> result { ((-1 + sqrt_d) * b / 2 / a), ((-1 - sqrt_d) * b / 2 / a) };
+        return result;
+    }
 }
